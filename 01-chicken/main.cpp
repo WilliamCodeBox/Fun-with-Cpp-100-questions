4b31fb05115006e5572552c78ac3c6c6a4f61778
Liu Chang <luroylau@163.com>
2020-04-29 10:17:51 +0800

    add code implementation

---
+++ 01-chicken/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  // Given condition
+  int maxNum = 100;
+  int maxMoney = 100;
+
+  // number of cock, hen, and chicken
+  int nCock;
+  int nHen;
+  int nChick;
+
+  // maximum number of cock, hen, and chicken we can buy with given money
+  int maxCock = int(maxMoney / 5);
+  int maxHen = int(maxMoney / 3);
+  int maxChick = int(maxMoney / 1);
+
+  // for loop to loop through all kinds of  combinations
+  for (nCock = 0; nCock <= maxCock; nCock++)
+  {
+    for (nHen = 0; nHen <= maxHen; nHen++)
+    {
+      for (nChick = 0; nChick <= maxChick; nChick++)
+      {
+        // check condition
+        if (nChick % 3 == 0)
+        {
+          int totalNumber = nCock + nHen + nChick;
+          int totalMoney = 5 * nCock + 3 * nHen + int(1 * nChick / 3);
+          if ((totalNumber == 100) && (totalMoney == 100))
+          {
+            std::cout << "Solution:\n";
+            std::cout << "Number of cock    = " << nCock << std::endl;
+            std::cout << "Number of Hen     = " << nHen << std::endl;
+            std::cout << "Number of Chicken = " << nChick << std::endl;
+          }
+        }
+      }
+    }
+  }
+
+  return 0;
+}
