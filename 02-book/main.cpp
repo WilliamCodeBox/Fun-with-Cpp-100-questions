73dec97bba212fe1449e7ba53c1bc4f16ac93d7a
Liu Chang <luroylau@163.com>
2020-04-30 09:19:33 +0800

    basic code implementation for problem 2

--- 02-book/main.cpp
+++ 02-book/main.cpp
@@ -3,5 +3,23 @@
 int main(int argc, char const *argv[])
 {
 
+  int bA;
+  int bB;
+  int bC;
+
+  for (bA = 1; bA <= 5; bA++)
+  {
+    for (bB = 1; bB <= 5; bB++)
+    {
+      for (bC = 1; bC <= 5; bC++)
+      {
+        if ((bA != bB) && (bB != bC) && (bC != bA))
+        {
+          std::cout << "A: " << bA << " B: " << bB << " C: " << bC << std::endl;
+        }
+      }
+    }
+  }
+
   return 0;
 }
